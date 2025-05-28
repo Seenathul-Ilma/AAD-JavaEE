ba7c8625fb7ead16af732fda03e353574b0d1b4e
ZeeanthulIlma <seenathulilma121243@gmail.com>
2025-05-28 19:10:58 +0530

    finalized

---
+++ MIME-Types/src/main/java/lk/ijse/gdse71/MimeTypes.java
@@ -0,0 +1,88 @@
+package lk.ijse.gdse71;
+
+
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.MultipartConfig;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.Part;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.List;
+import java.util.stream.Collectors;
+
+@WebServlet("/mime")
+@MultipartConfig
+public class MimeTypes extends HttpServlet {
+    // 1
+    /*@Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        //read text/plain data from http request body  - raw
+        String body = new BufferedReader(new InputStreamReader(
+                req.getInputStream())).
+                lines().collect(Collectors.joining("\n"));
+
+        resp.setContentType("text/plain");
+        resp.getWriter().write(body);
+    }*/
+
+    // 2
+    /*@Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        String name = req.getParameter("name");
+        String address = req.getParameter("address");
+
+        resp.setContentType("text/plain");
+        resp.getWriter().write(name + " - " + address);
+    }*/
+
+    // 3
+    /*@Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        String name = req.getParameter("name");
+        Part part = req.getPart("file");
+        String fileName = part.getSubmittedFileName();
+        resp.setContentType("text/plain");
+        resp.getWriter().write(name + " - " + fileName);
+    }*/
+
+    // 4
+    // raw - JSON
+    // JSON Object
+    // read JSON data form httpRequest body
+    /*@Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        ObjectMapper mapper = new ObjectMapper();
+        JsonNode jsonNode = mapper.readTree(req.getReader());
+
+        String name = jsonNode.get("name").asText();
+        String address = jsonNode.get("address").asText();
+
+        resp.setContentType("text/plain");
+        resp.getWriter().write(name+" - "+address);
+    }*/
+
+    // 5
+    // raw - JSON
+    // JSON Array
+    // read JSON Array data form httpRequest body
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        ObjectMapper mapper = new ObjectMapper();
+        List<JsonNode> users = mapper.readValue(req.getReader(), new TypeReference<List<JsonNode>>(){});
+        for (JsonNode user : users) {
+            String name = user.get("name").asText();
+            String address = user.get("address").asText();
+            System.out.println(name + " - " + address);
+        }
+        resp.setContentType("text/plain");
+        resp.getWriter().println("OK");
+    }
+}
---
+++ Mapping-Specification/src/main/java/lk/ijse/gdse71/ExtensionMappingServlet.java
@@ -0,0 +1,28 @@
+package lk.ijse.gdse71;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+
+/**
+ * --------------------------------------------
+ * Author: Zeenathul Ilma
+ * GitHub: https://github.com/Seenathul-Ilma
+ * Website: https://zeenathulilma.vercel.app/
+ * --------------------------------------------
+ * Created: 5/22/2025 10:29 AM
+ * Project: Examples
+ * --------------------------------------------
+ **/
+
+@WebServlet("*.txt")
+public class ExtensionMappingServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        resp.getWriter().print("Extension Mapping Servlet");
+    }
+}
